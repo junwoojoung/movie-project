7a13d8471ee9a7f41f626ba7c09e4ca185c91240
jsjw39073907 <junwoo0112@naver.com>
2021-05-28 17:41:47 +0900

    docs: Controller
    회원정보 삭제 페이지에서 form을 ajax로 변경하면서 회원삭제 페이지에서 비밀번호 확인 작업을 진행하지 않고
    pwChk를 매핑하여 비밀번호 확인

--- MovieController.java
+++ MovieController.java
@@ -179,26 +179,34 @@ public class MovieController {
 	public String memberDelete(UserInfoVO vo, HttpSession session, RedirectAttributes rttr) throws Exception {
 		LOGGER.info("post : 회원정보 삭제 페이지");
 		LOGGER.info(vo.toString());
-
 		// 세션에 있는 member를 가져와 member 변수에 넣는다
-		UserInfoVO member = (UserInfoVO) session.getAttribute("member");
+//		UserInfoVO member = (UserInfoVO) session.getAttribute("member");
 		// 세션에 있는 비밀번호
-		String sessionPw = member.getUserPw();
+//		String sessionPw = member.getUserPw();
 		// vo로 들어오는 비밀번호
-		String voPw = vo.getUserPw();
-		LOGGER.info("세션 비밀번호 : " + sessionPw);
-		LOGGER.info("vo 비밀번호 : " + voPw);
-
-		if (!(sessionPw.equals(voPw))) {
-			rttr.addFlashAttribute("msg", false);
-			return "redirect:/movie/info-delete";
-		}
-
+//		String voPw = vo.getUserPw();
+//		LOGGER.info("세션 비밀번호 : " + sessionPw);
+//		LOGGER.info("vo 비밀번호 : " + voPw);
+//
+//		if (!(sessionPw.equals(voPw))) {
+//			rttr.addFlashAttribute("msg", false);
+//			return "redirect:/movie/info-delete";
+//		}
 		movieservice.memberDelete(vo);
 
 		session.invalidate();
 
 		return "redirect:/movie/index";
+	}
+
+	// 비밀번호 체크
+	@ResponseBody
+	@PostMapping("/pwChk")
+	public int pwChk(UserInfoVO vo) throws Exception {
+		LOGGER.info("Controller : " + vo.toString());
+		int result = movieservice.pwChk(vo);
+		LOGGER.info("리턴 값" + result);
+		return result;
 	}
 
 
