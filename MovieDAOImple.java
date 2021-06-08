47064811e26ec969faa8f5cc97fd5dc3abee884c
jsjw39073907 <junwoo0112@naver.com>
2021-06-08 21:56:05 +0900

    feat: DAOImple

--- MovieDAOImple.java
+++ MovieDAOImple.java
@@ -1,8 +1,8 @@
 package goott.spring.project1.persistence;
 
+import java.util.HashMap;
 import java.util.List;
-
-import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import goott.spring.project1.domain.MovieVO;
+import goott.spring.project1.domain.ReserveVO;
+import goott.spring.project1.domain.RunningTimeVO;
+import goott.spring.project1.domain.TheaterVO;
 import goott.spring.project1.domain.UserInfoVO;
 
 @Repository
@@ -23,16 +26,37 @@ public class MovieDAOImple implements MovieDAO{
 
 	@Override
 	public List<MovieVO> select() throws Exception {
-		LOGGER.info("관객수가 많은 1~4 순서대로 검색");
+		LOGGER.info("1~4위 영화 리스트");
 		return sqlSession.selectList(NAMESPACE + ".select");
+	}
+
+	@Override
+	public List<MovieVO> allSelect() throws Exception {
+		LOGGER.info("전체 영화 리스트");
+		return sqlSession.selectList(NAMESPACE + ".allSelect");
+	}
+
+	@Override
+	public List<TheaterVO> theaterList() throws Exception {
+		LOGGER.info("상영관 정보 가져오기");
+		return sqlSession.selectList(NAMESPACE + ".theater");
+	}
+
+	@Override
+	public MovieVO getPoster(String movieName) throws Exception {
+		return sqlSession.selectOne(NAMESPACE + ".getPoster", movieName);
+	}
+
+	@Override
+	public List<RunningTimeVO> runningTimeInfo(RunningTimeVO vo) {
+		return sqlSession.selectList(NAMESPACE + ".runningTime", vo);
 	}
 
 	@Override
 	public void insert(UserInfoVO vo) throws Exception {
 		LOGGER.info("입력한 정보 삽입");
-		sqlSession.insert(NAMESPACE + ".insert", vo);
+		sqlSession.insert(NAMESPACE + ".memberInsert", vo);
 	}
-
 
 	@Override
 	public int idChk(UserInfoVO vo) throws Exception{
@@ -41,21 +65,48 @@ public class MovieDAOImple implements MovieDAO{
 	}
 
 	@Override
-	public boolean login(UserInfoVO vo) throws Exception {
-		LOGGER.info("로그인 확인 기능 처리");
-		int result = sqlSession.selectOne(NAMESPACE + ".login", vo);
-		LOGGER.info("DAOIple 확인 : " + result);
-
-		// 검색이 안 되면 0을 반환해주기 때문에 0과 비교해서 참이면 false, 거짓이면 true를 반환
-		return (result == 0)? false:true;
+	public UserInfoVO login(String userId, String userPw) throws Exception{
+		LOGGER.info("dao imple");
+		LOGGER.info("userId : " + userId);
+		LOGGER.info("userPw : " + userPw);
+		Map<String, String> chk = new HashMap<String, String>();
+		chk.put("userId", userId);
+		chk.put("userPw", userPw);
+		return sqlSession.selectOne(NAMESPACE + ".login", chk);
 
 	}
 
 	@Override
-	public void logOut(HttpSession session) throws Exception {
-		LOGGER.info("로그아웃 기능 처리");
-		session.invalidate();
-		LOGGER.info("세션 삭제 성공");
+	public void memberUpdate(UserInfoVO vo) throws Exception {
+		LOGGER.info("dao imple");
+		LOGGER.info("vo : " + vo.toString());
+		sqlSession.update(NAMESPACE + ".memberUpdate", vo);
 	}
+
+	@Override
+	public void memeberDelete(UserInfoVO vo) throws Exception {
+		LOGGER.info("dao imple");
+		LOGGER.info("vo : " + vo.toString());
+		sqlSession.update(NAMESPACE + ".memberDelete", vo);
+	}
+
+	@Override
+	public int pwChk(UserInfoVO vo) throws Exception {
+		LOGGER.info("dao imple");
+		LOGGER.info("vo : " + vo.toString());
+		int result = sqlSession.selectOne(NAMESPACE + ".pwChk", vo);
+		return result;
+	}
+
+	@Override
+	public List<ReserveVO> reserveSelect() throws Exception {
+		LOGGER.info("dao imple");
+		return sqlSession.selectList(NAMESPACE + ".reserve");
+	}
+
+
+
+
+
 
 }
