6d84bd0d19323fac5ef9ed26ceff8f16140f0b0b
jsjw39073907 <junwoo0112@naver.com>
2021-06-08 21:55:45 +0900

    feat: DAO

--- MovieDAO.java
+++ MovieDAO.java
@@ -2,26 +2,50 @@ package goott.spring.project1.persistence;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import goott.spring.project1.domain.MovieVO;
+import goott.spring.project1.domain.ReserveVO;
+import goott.spring.project1.domain.RunningTimeVO;
+import goott.spring.project1.domain.TheaterVO;
 import goott.spring.project1.domain.UserInfoVO;
 
 public interface MovieDAO {
 	// 1~4위 영화 검색
 	public abstract List<MovieVO> select() throws Exception;
 
-	// 회원가입
+	// 모든 영화 목록
+	public abstract List<MovieVO> allSelect() throws Exception;
+
+	// 상영관 정보
+	public abstract List<TheaterVO> theaterList() throws Exception;
+
+	// 포스터 출력
+	public  abstract MovieVO getPoster(String movieName) throws Exception;
+
+	// 상영시간표
+	public abstract List<RunningTimeVO> runningTimeInfo(RunningTimeVO vo) throws Exception;
+
+	// 회원정보 삽입
 	public abstract void insert(UserInfoVO vo) throws Exception;
 
 	// 아이디 중복 확인
 	public abstract int idChk(UserInfoVO vo) throws Exception;
 
 	// 로그인
-	public abstract boolean login(UserInfoVO vo) throws Exception;
+	public abstract UserInfoVO login(String userId, String userPw) throws Exception;
 
-	// 로그아웃
-	public abstract void logOut(HttpSession session) throws Exception;
+	// 회원정보 수정
+	public abstract void memberUpdate(UserInfoVO vo) throws Exception;
+
+	// 회원정보 삭제
+	public abstract void memeberDelete(UserInfoVO vo) throws Exception;
+
+	// 비밀번호 체크
+	public abstract int pwChk(UserInfoVO vo) throws Exception;
+
+	// 예매정보
+	public abstract List<ReserveVO> reserveSelect() throws Exception;
+
+
 
 
 }
